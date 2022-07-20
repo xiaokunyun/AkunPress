dddee9711310ac92cc4809afe1d86e2d2f084c2a
xuxiaokun <xuxiaokun@cnrmall.com>
2022-07-20 17:35:55 +0800

    1

---
+++ .vitepress/config.ts
@@ -0,0 +1,92 @@
+/**
+ * @type {import('vitepress').UserConfig}
+ */
+import { defineConfig } from 'vitepress'
+export default defineConfig({
+    base: '/base/',
+    title: 'AkunPress',
+    lang: 'zh-CN',
+    description: 'Just playing around.',
+    appearance: true,
+    themeConfig: {
+        // siteTitle: 'My Custom Title',
+        logo: '/logo.png',
+        // siteTitle: false,
+        nav: createNav(),// 头部导航栏
+        sidebar: createSidebar(),//侧边栏
+        socialLinks: [
+            { icon: 'github', link: 'https://github.com/xiaokunyun/vitepress' },
+            { icon: 'twitter', link: '...' },
+            { icon: 'discord', link: '...' }
+        ],
+        //页脚
+        footer: {
+            message: '在 MIT 许可下发布。',
+            copyright: '版权所有 © 2022-至今 Evan You'
+        }
+    }
+})
+function createNav() {
+    return [
+        {
+            text: '指南', link: '/guide/',
+            items: [
+                {
+                    text: '指南',
+                    link: '/guide/introduction',
+                },
+                {
+                    text: 'test',
+                    link: '/guide/test',
+                },
+            ]
+        },
+        // { text: '团队', link: '/team/' },
+        { text: '相关', link: '/relevant/' },
+        {
+            text: '相关链接',
+            items: [
+                { text: 'press', link: 'http://press.xiaokunyun.cn/' },
+                { text: 'web', link: 'http://web.xiaokunyun.cn/' },
+                { text: '仿网易云api', link: 'http://node.xiaokunyun.cn/' }
+            ]
+        },
+    ]
+}
+function createSidebar() {
+    return {
+        '/guide/': [
+            {
+                text: '指南',
+                collapsible: true,
+                items: [
+                    { text: 'index', link: '/guide/index.md' },
+                    { text: 'Introduction', link: '/guide/introduction.md' },
+                    { text: 'one', link: '/guide/one.md' },
+                    { text: 'two', link: '/guide/two.md' },
+                    { text: 'test', link: '/guide/test.md' },
+                ]
+            }
+        ],
+        // '/team/': [
+        //     {
+        //         text: '团队',
+        //         collapsible: true,
+        //         items: [
+        //             { text: 'index', link: '/team/index.md' },
+        //         ]
+        //     }
+        // ],
+        '/relevant/': [
+            {
+                text: 'relevant',
+                collapsible: true,
+                items: [
+                    { text: 'index', link: '/relevant/index.md' },
+                    { text: 'one', link: '/relevant/one.md' },
+                    { text: 'two ', link: '/relevant/two.md' },
+                ]
+            }
+        ]
+    }
+}
---
+++ index.md
@@ -0,0 +1,39 @@
+---
+layout: home
+hero:
+  name: VitePress
+  text: Vite & Vue powered static site generator.
+  tagline: Lorem ipsum...
+  image:
+    src: /logo.png
+    alt: VitePress
+  actions:
+    - theme: brand
+      text: 开始 使用
+      link: /guide/index
+    - theme: alt
+      text: View on GitHub
+      link: https://github.com/xiaokunyun/vitepress
+# home: true
+# heroImage: /logo.png
+# actionText: 快速开始 →
+# actionLink: /guide/introduction
+
+# altActionText: 在线预览
+# altActionLink: https://vvbin.cn/next/
+
+features:
+  - title: 💡 最新技术栈
+    details: 基于VitePress、Vue3、Vite、TypeScript等最新技术栈开发
+  - title: ⚡️ 轻量快速的热重载
+    details: 无论应用程序大小如何，都始终极快的模块热重载（HMR）
+  - title: 🛠️ 丰富的示例
+    details: 常见的Web端插件示例实现
+  # - title: 📦 组件封装
+  #   details: 对日常使用频率较高的组件二次封装,满足基础工作需求
+  # - title: 🔩 主题配置
+  #   details: 丰富的主题配置及黑暗主题适配
+  # - title: 🔑 权限管理
+  #   details: 完善的前后端权限管理方案
+footer: MIT Licensed | Copyright © 2021-present Vben
+---
