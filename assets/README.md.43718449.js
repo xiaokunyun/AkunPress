6e30ad54ae67368f8558871d1f819d152ad78e86
xiaokunyun <xiaokunyun@users.noreply.github.com>
2022-07-21 08:00:49 +0000

    deploy: fd103ce362f3e755e247708d7bf03af0a9709422

---
+++ assets/README.md.43718449.js
@@ -0,0 +1,9 @@
+import{_ as a,c as n,o as s,a as e}from"./app.da443b3b.js";const u=JSON.parse('{"title":"vue-vben-admin-doc","description":"","frontmatter":{},"headers":[{"level":2,"title":"\u5982\u4F55\u672C\u5730\u5F00\u53D1","slug":"\u5982\u4F55\u672C\u5730\u5F00\u53D1"}],"relativePath":"README.md"}'),t={name:"README.md"},l=e(`<h1 id="vue-vben-admin-doc" tabindex="-1">vue-vben-admin-doc <a class="header-anchor" href="#vue-vben-admin-doc" aria-hidden="true">#</a></h1><h2 id="\u5982\u4F55\u672C\u5730\u5F00\u53D1" tabindex="-1">\u5982\u4F55\u672C\u5730\u5F00\u53D1 <a class="header-anchor" href="#\u5982\u4F55\u672C\u5730\u5F00\u53D1" aria-hidden="true">#</a></h2><div class="language-bash"><span class="copy"></span><pre><code><span class="line"><span style="color:#676E95;font-style:italic;"># \u514B\u9686\u672C\u4ED3\u5E93</span></span>
+<span class="line"><span style="color:#676E95;font-style:italic;"># $ git clone git@github.com:vbenjs/vue-vben-admin-doc.git</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#676E95;font-style:italic;"># \u6216\u8005\u4F7F\u7528 yarn</span></span>
+<span class="line"><span style="color:#A6ACCD;">$ yarn install</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#676E95;font-style:italic;"># \u542F\u52A8\u5F00\u53D1\u670D\u52A1\u5668</span></span>
+<span class="line"><span style="color:#A6ACCD;">$ yarn dev</span></span>
+<span class="line"></span></code></pre></div>`,3),c=[l];function i(o,p,r,d,_,h){return s(),n("div",null,c)}var m=a(t,[["render",i]]);export{u as __pageData,m as default};
