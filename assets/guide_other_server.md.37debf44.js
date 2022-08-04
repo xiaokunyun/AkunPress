10f76bb53d1b2c4049d3693624ecf187aefb5dc5
xiaokunyun <xiaokunyun@users.noreply.github.com>
2022-08-04 12:19:03 +0000

    deploy: 01f447e25e80e946540a6a0556f1e65c8c3384db

---
+++ assets/guide_other_server.md.37debf44.js
@@ -0,0 +1,10 @@
+import{_ as s,o as a,c as e,d as n}from"./app.ca3c8b07.js";const u=JSON.parse('{"title":"\u6D4B\u8BD5\u670D\u52A1\u5668","description":"","frontmatter":{},"headers":[{"level":2,"title":"\u4F7F\u7528","slug":"\u4F7F\u7528"}],"relativePath":"guide/other/server.md"}'),t={name:"guide/other/server.md"},l=n(`<h1 id="\u6D4B\u8BD5\u670D\u52A1\u5668" tabindex="-1">\u6D4B\u8BD5\u670D\u52A1\u5668 <a class="header-anchor" href="#\u6D4B\u8BD5\u670D\u52A1\u5668" aria-hidden="true">#</a></h1><p>\u5728\u9879\u76EE <code>/test/server</code> \u5185\u6709\u7B80\u5355\u7684 Node.js \u6D4B\u8BD5\u540E\u53F0\u63A5\u53E3\u670D\u52A1\uFF0C\u7528 Koa2 \u5B9E\u73B0</p><h2 id="\u4F7F\u7528" tabindex="-1">\u4F7F\u7528 <a class="header-anchor" href="#\u4F7F\u7528" aria-hidden="true">#</a></h2><div class="language-bash"><span class="copy"></span><pre><code><span class="line"></span>
+<span class="line"><span style="color:#82AAFF;">cd</span><span style="color:#A6ACCD;"> ./test/server</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#676E95;font-style:italic;"># \u5B89\u88C5\u4F9D\u8D56</span></span>
+<span class="line"><span style="color:#A6ACCD;">yarn</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#676E95;font-style:italic;"># \u8FD0\u884C\u670D\u52A1</span></span>
+<span class="line"><span style="color:#A6ACCD;">yarn start</span></span>
+<span class="line"></span>
+<span class="line"></span></code></pre></div><p>\u670D\u52A1\u8FD0\u884C\u6210\u529F\u4E4B\u540E\uFF0C\u5C31\u53EF\u4EE5\u8BBF\u95EE\u6D4B\u8BD5\u4E0A\u4F20\u63A5\u53E3\u53CA websocket \u63A5\u53E3\u670D\u52A1</p>`,5),r=[l];function p(c,o,i,d,_,h){return a(),e("div",null,r)}var v=s(t,[["render",p]]);export{u as __pageData,v as default};
